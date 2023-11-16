723fa8c241b175d867e67578614b53e9c193f389
Onym09 <150085376+Onym09@users.noreply.github.com>
2023-11-16 11:12:08 -0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+function me() {
+    document.getElementById("ghost").style.display="block"
+    alert("boo!")
+
+}
+
+function show() {
+    document.getElementById("meme").style.display="block"
+
+}
+
